795e327e3c8e2893143dc54d4bae6488b8cbf377
odaiamer-1997 <133056286+odaiamer-1997@users.noreply.github.com>
2023-05-10 21:35:02 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productRoutes = require('./routes/product');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(productRoutes);
+
+app.listen(8080);
